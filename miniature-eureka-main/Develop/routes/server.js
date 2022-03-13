8d47d8ba30389edd97d99b35a9a264f1ae9117ce
noellecrow <crownoelle@gmail.com>
2022-03-13 14:06:50 -0700

    added get functions

--- miniature-eureka-main/Develop/routes/server.js
+++ miniature-eureka-main/Develop/routes/server.js
@@ -22,6 +22,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 function createNewNote(body, notesArray) {
     const newNote = body;
     if (!Array.isArray(notesArray))
