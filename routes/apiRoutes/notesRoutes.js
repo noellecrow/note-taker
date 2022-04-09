53ea3c8afe790f2950fc7e654e8e514d3871d165
noellecrow <crownoelle@gmail.com>
2022-04-09 13:50:10 -0700

    added post route

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -9,6 +9,14 @@ router.get('/notes', (req, res) => {
     res.json(createNewNote(req.body, notesArray));
 });
 
+router.post('/notes', (req, res) => {
+    if(notesArray){
+        req.body.id = notesArray.length.toString();
+    } else
+    {req.body.id =0}
+    res.json(createNewNote(req.body, notesArray));
+});
+
 router.delete('/notes/:id', async (req, res) => {
     const { id } = req.params
     notesArray = await deleteNote(id, notesArray);
