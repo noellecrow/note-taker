593fa335fa52903f538951b4706130ff43cc988a
noellecrow <crownoelle@gmail.com>
2022-04-17 12:08:03 -0700

    edited server.js

--- server.js
+++ server.js
@@ -1,101 +1,21 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 
-// Variable for Express
-const app = express();
-// Define a port to listen for
 const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 
-// Middleware
+// Parse URL encoded and JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Host public folder
 app.use(express.static('public'));
 
 // Setting routes for APIs
-app.get('/api/notes', (req, res) => {
-
-fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data);
-        res.json(JSON.parse(data));
-
-    }
-})
-});
-
-
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-
-// Functions to add notes
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-    notesArray = [];
-
-    if(notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-}
-
-app.post('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const newNote = createNewNote(req.body, data);
-            res.json(newNote);
-        }
-    })
-
-});
-
-// Function to delete notes
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
-
-        if(note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
-
-            break;
-        }
-    }
-}
-
-app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, allNotes);
-    res.json(true);
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server is now on port ${PORT}!`);
